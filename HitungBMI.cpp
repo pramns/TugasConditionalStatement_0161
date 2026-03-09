1acafce066afd4803c50ef8f4c894b000d462b24
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-09 20:36:16 +0700

    Menambah kondisi

--- HitungBMI.cpp
+++ HitungBMI.cpp
@@ -11,3 +11,15 @@ void inputdata(float b,float t){
 float hitungBMI (float a, float b){
   return a/(b*b);
 }
+
+string kondisiBMI(float bmi){
+  if (bmi < 18.5){
+    return "Berat Badan Kurang";
+  }else if (bmi < 25){
+    return "Berat Badan Normal";
+  }else if (bmi < 30){
+    return "Berat Badan Kelebihan";
+  }else{
+    return "Obesitas";
+  }
+}
