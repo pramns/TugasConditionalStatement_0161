8dac90af71f7d685044cfe9a85f4d5b132d389aa
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-09 20:23:29 +0700

    Menambah input data

--- HitungBMI.cpp
+++ HitungBMI.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void inputdata(float b,float t){
+  cout << "Masukan Berat badan anda = ";
+  cin >> b;
+  cout << "Masukan Tinggi badan anda = ";
+  cin >> t;
+}
+
